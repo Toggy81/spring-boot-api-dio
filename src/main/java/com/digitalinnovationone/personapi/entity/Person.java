4a75752b50a22075f4ce703c097bc9c74fe2dfd2
Toggy <tognolamontes@yahoo.com.br>
2021-04-21 17:59:01 -0300

    Mapped entities

--- src/main/java/com/digitalinnovationone/personapi/entity/Person.java
+++ src/main/java/com/digitalinnovationone/personapi/entity/Person.java
@@ -1,5 +1,29 @@
 package com.digitalinnovationone.personapi.entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+	
+	private Long id;
+	
+	private String firstName;
+	
+	private String LastName;
+	
+	private String cpf;
+	
+	private LocalDate birthDate;
+	
+	private List<Phone> phones;
 
 }
