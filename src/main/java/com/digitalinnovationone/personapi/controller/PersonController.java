023b60ee0d5c3dabb230e84ccbe49a4e0e05eb11
Toggy <tognolamontes@yahoo.com.br>
2021-04-21 18:27:30 -0300

    Implement Post method to person controller

--- src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -1,16 +1,28 @@
 package com.digitalinnovationone.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.digitalinnovationone.personapi.entity.Person;
+import com.digitalinnovationone.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 	
-	@GetMapping
-	public String getBook() {
-		return "API Test";
+	
+	private PersonRepository personRepository;	
+	
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	@PostMapping
+	public void createPerson(Person person) {
+		personRepository.save(person);
 	}
 
 }
