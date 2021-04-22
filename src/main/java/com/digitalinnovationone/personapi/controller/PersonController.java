56c600526c6d1d6896f18396c4233edf5039c79e
Toggy <tognolamontes@yahoo.com.br>
2021-04-22 17:50:16 -0300

    Add MessageResponseDTO class

--- src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -1,7 +1,9 @@
 package com.digitalinnovationone.personapi.controller;
 
+import com.digitalinnovationone.personapi.dto.MessageResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,8 @@ import com.digitalinnovationone.repository.PersonRepository;
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-	
-	
-	private PersonRepository personRepository;	
+
+	private PersonRepository personRepository;
 	
 	@Autowired
 	public PersonController(PersonRepository personRepository) {
@@ -21,8 +22,13 @@ public class PersonController {
 	}
 
 	@PostMapping
-	public void createPerson(Person person) {
-		personRepository.save(person);
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+
+		Person savedPerson = personRepository.save(person);
+		return MessageResponseDTO
+				.builder()
+				.message("Created person with ID " + savedPerson.getId())
+				.build();
 	}
 
 }
