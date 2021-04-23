a42c2077d1337ff4ba4b047671baf4abac87943b
Toggy <tognolamontes@yahoo.com.br>
2021-04-23 17:29:51 -0300

    Add MapStruct dependencie

--- src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovationone/personapi/controller/PersonController.java
@@ -1,28 +1,27 @@
 package com.digitalinnovationone.personapi.controller;
 
 import com.digitalinnovationone.personapi.dto.MessageResponseDTO;
+import com.digitalinnovationone.personapi.dto.request.PersonDTO;
 import com.digitalinnovationone.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import com.digitalinnovationone.personapi.entity.Person;
-
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-	@Autowired
 	private final PersonService personService;
 
+	@Autowired
 	public PersonController(PersonService personService) {
 		this.personService = personService;
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public MessageResponseDTO createPerson(@RequestBody Person person) {
-		return personService.createPerson(person);
+	public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
+		return personService.createPersonDTO(personDTO);
 	}
 
 }
